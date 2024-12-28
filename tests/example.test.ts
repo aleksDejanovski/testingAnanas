0d85d5db884adf7de41f611f580a190799d82ff3
dejanovski_a@yahoo.com <dejanovski_a@yahoo.com>
2024-12-28 20:13:34 +0100

    add data driven tests

--- tests/example.test.ts
+++ tests/example.test.ts
@@ -1,6 +1,7 @@
 import { test, expect } from "@playwright/test";
 import DashBoardPage from "./DashBoardPage";
 import LoginPage from "./LoginPage";
+//import * as userData from "../tests-examples/dataForTest/data.json";
 
 test("login using valid credentals", async ({ page }) => {
   const dashBoardPage = new DashBoardPage(page);
@@ -46,4 +47,40 @@ test("login using valid credentals and search for product", async ({
   await dashBoardPage.AleksandarLogedIn();
   await dashBoardPage.SearchItem("test za bremenost");
   await dashBoardPage.ResutsTestBremenost();
+});
+
+test("login using valid credentals from data file and search for product", async ({
+  page,
+}) => {
+  const userData = require("../tests-examples/dataForTest/data.json");
+  const dashBoardPage = new DashBoardPage(page);
+  const loginPage = new LoginPage(page);
+  await dashBoardPage.GoTo();
+  await dashBoardPage.ClickIAgree();
+  await dashBoardPage.ClickAvatar();
+  //await dashBoardPage.ClickNajaviSe();
+  await loginPage.EnterCredentialsAndSubmit(
+    userData[0].username,
+    userData[0].password
+  );
+  await dashBoardPage.AleksandarLogedIn();
+  await dashBoardPage.SearchItem("test za bremenost");
+  await dashBoardPage.ResutsTestBremenost();
+});
+
+test("login using invalid credentals from data file and search for product", async ({
+  page,
+}) => {
+  const userData = require("../tests-examples/dataForTest/data.json");
+  const dashBoardPage = new DashBoardPage(page);
+  const loginPage = new LoginPage(page);
+  await dashBoardPage.GoTo();
+  await dashBoardPage.ClickIAgree();
+  await dashBoardPage.ClickAvatar();
+  //await dashBoardPage.ClickNajaviSe();
+  await loginPage.EnterCredentialsAndSubmit(
+    userData[1].username,
+    userData[1].password
+  );
+  await loginPage.NotValidLogin();
 });
